f39550e12c92453ca521819e52c04b195361fd82
wtereshko <42153162+wtereshko@users.noreply.github.com>
2020-02-06 13:02:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Weather_Forecast
+{
+   public class Program
+    {
+        /*2) Solve the following task using any convenient programming language
+         Solve the following task using any convenient programming language
+         Let’s imagine that you receive results of the weather for today in the following format: ‘+26C’. 
+         Weather results can change.Please write part of the code which will return the following: 
+         a) If the weather is from -50C to -1C return “It’s super cold today.Be sure you dressed well!”
+         b) If the weather is from -0C to +10C return “It’s windy outside, but we are sure you will enjoy your day!”
+         c) If the weather is from +11C to +30C return “It’s time for the outdoor walking!”
+         d) If the weather is from +31 to 40 return “It's so hot outside!”
+         e) If the weather is from +41 to 50 return return “Welcome to hell!”
+         f) If the weather results do not match above values return “Please re-check results in 5 mins.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value");
+            Console.WriteLine(GetResult(Console.ReadLine()));
+        }
+
+        public static string GetResult(string receiveResult)
+        {
+            double weatherValue;
+            string receiveResultValue = Regex.Replace(receiveResult, @"[^0-9,.-]+", "");
+
+            if (Double.TryParse(receiveResultValue.Replace('.', ','), out weatherValue))
+            {
+                return CheckWeather(weatherValue);
+            }
+            else
+            {
+                return "Please re-check results in 5 mins.";
+            }
+        }
+
+        public static string CheckWeather(double weatherResult)
+        {
+            if (weatherResult >= -50 && weatherResult <= -1)
+            {
+                return "It’s super cold today.Be sure you dressed well!";
+            }
+
+            if (weatherResult >= 0 && weatherResult <= 10)
+            {
+                return "It’s windy outside, but we are sure you will enjoy your day!";
+            }
+
+            if (weatherResult >= 11 && weatherResult <= 30)
+            {
+                return "It’s time for the outdoor walking!";
+            }
+
+            if (weatherResult >= 31 && weatherResult <= 40)
+            {
+                return "It's so hot outside!";
+            }
+
+            if (weatherResult >= 41 && weatherResult <= 50)
+            {
+                return "Welcome to hell!";
+            }
+
+            return "Please re-check results in 5 mins.";
+        }
+    }
+}
---
+++ ProgramTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Weather_Forecast
+{
+    [TestFixture]
+   public class ProgramTest
+    {
+        [TestCase("'-39C'")]
+        [TestCase("'fg-1,1C")]
+        public void GetResults_Test_ColdWeather(string weatherResult)
+        {
+            string expectedResult = "It’s super cold today.Be sure you dressed well!";
+            string actualResult = Program.GetResult(weatherResult);
+            
+            StringAssert.AreEqualIgnoringCase(expectedResult, actualResult);
+        }
+
+        [TestCase("'0C'")]
+        [TestCase("'+9C")]
+        public void GetResults_Test_WindyWeather(string weatherResult)
+        {
+            string expectedResult = "It’s windy outside, but we are sure you will enjoy your day!";
+            string actualResult = Program.GetResult(weatherResult);
+
+            StringAssert.AreEqualIgnoringCase(expectedResult, actualResult);
+        }
+       
+        [TestCase("'+15C'")]
+        [TestCase("test'+29,9C")]
+        public void GetResults_Test_PerfectWeather(string weatherResult)
+        {
+            string expectedResult = "It’s time for the outdoor walking!";
+            string actualResult = Program.GetResult(weatherResult);
+
+            StringAssert.AreEqualIgnoringCase(expectedResult, actualResult);
+        }
+
+        [TestCase("'+31C'")]
+        [TestCase("38.2C")]
+        public void GetResults_Test_HotWeather(string weatherResult)
+        {
+            string expectedResult = "It's so hot outside!";
+            string actualResult = Program.GetResult(weatherResult);
+
+            StringAssert.AreEqualIgnoringCase(expectedResult, actualResult);
+        }
+
+        [TestCase("'+41C'")]
+        [TestCase("+50C")]
+        public void GetResults_Test_HellWeather(string weatherResult)
+        {
+            string expectedResult = "Welcome to hell!";
+            string actualResult = Program.GetResult(weatherResult);
+
+            StringAssert.AreEqualIgnoringCase(expectedResult, actualResult);
+        }
+
+        [TestCase("'jkvjknjasxuyn'")]
+        [TestCase("@#$!$%-+C\'")]
+        public void GetResults_Test_ReCheck(string weatherResult)
+        {
+            string expectedResult = "Please re-check results in 5 mins.";
+            string actualResult = Program.GetResult(weatherResult);
+
+            StringAssert.AreEqualIgnoringCase(expectedResult, actualResult);
+        }
+    }
+}
